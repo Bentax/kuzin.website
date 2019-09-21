3bf108a4c5ac42dfd0aeef8147030f421ad5ceb8
Andrey Kuzin <51067882+Bentax@users.noreply.github.com>
2019-09-21 13:59:19 +0700

    some3

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 $(function(){
     $('.order-recall').on('click', function(){
-        $('#form-container').css('display', 'flex');
+        $('.form-container').css('display', 'flex');
     });
 
-    $('#form-container').on('click', function(event){
+    $('.form-container').on('click', function(event){
         if(event.target == this) {
-        $('#form-container').css('display', 'none');//скрытие формы
+        $('.form-container').css('display', 'none');//скрытие формы
             //скрывается контейнер
         }
     });
 
-    $('#recall-form').on('submit', function () {
+    $('.recall-form').on('submit', function () {
 
         let data = {
             'name': $(this).find('#name').val(),
@@ -20,8 +20,8 @@ $(function(){
         $.post('/api.php', data, function (response) {
             if(response == 1)
             {
-            $('#recall-form input[type="text"]').val('');
-            $('#form-container').css('display', 'none');
+            $('.recall-form input[type="text"]').val('');
+            $('.form-container').css('display', 'none');
             }
         });
         return false;
